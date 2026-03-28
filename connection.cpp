4376812fe04d98d03edfe0fe1a00a09180b4bc15
amine-lbabda <mohamedamine.lbabda@esprit.tn>
2026-03-29 00:10:41 +0100

    Adding support windows

--- connection.cpp
+++ connection.cpp
@@ -29,19 +29,26 @@ Connection &Connection::createInstance()
  */
 bool Connection::createConnection()
 {
-    //dotenv::init("/home/amine/Desktop/WoodSync-OCI/.env");
-    /*QString name = QString::fromUtf8(dotenv::getenv("DATABASE_NAME"));
+    // Guardrails for loading environment variables based on OS
+    #if defined(Q_OS_WIN)
+        dotenv::init("C:\\Users\\faycel\\Desktop\\personal_projects\\WoodSync-OCI\\.env");
+    #elif defined(Q_OS_LINUX)
+        dotenv::init("/home/amine/Desktop/WoodSync-OCI/.env");
+    #else
+        #error "Unsupported OS for dotenv initialization. Please add the correct path."
+    #endif
+    QString name = QString::fromUtf8(dotenv::getenv("DATABASE_NAME"));
     qDebug() << name;
     QString username = QString::fromUtf8(dotenv::getenv("DATABASE_USERNAME"));
     qDebug() << username;
     QString hostname = QString::fromUtf8(dotenv::getenv("DATABSE_HOST"));
     qDebug() << hostname;
     QString password = QString::fromUtf8(dotenv::getenv("DATABASE_PASSWORD"));
-    qDebug() << password;*/
-    db.setDatabaseName("Projet_2A"); //remplacer avec votre nom du projet
-    db.setUserName("amine"); //remplacer avec votre nom d'utilisateur
+    qDebug() << password;
+    db.setDatabaseName(name); //remplacer avec votre nom du projet
+    db.setUserName(username); //remplacer avec votre nom d'utilisateur
     //db.setHostName(hostname); //remplacer avec localhost ou 127.0.0.1
-    db.setPassword("admin"); //remplacer avec votre mdp
+    db.setPassword(password); //remplacer avec votre mdp
     if (db.open()){
         qDebug() << "Connection established !";
         return true;
